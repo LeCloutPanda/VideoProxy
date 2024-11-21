6e3a15c89bfce5378ad2e1e95eafcdd371883f3a
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2024-11-22 09:20:37 +1100

    More!

--- VideoProxy.cs
+++ VideoProxy.cs
@@ -11,12 +11,6 @@ using System.Text;
 
 public class VideoProxy : ResoniteMod
 {
-    public enum DebugLevel
-    {
-        None,
-        Min,
-        Max
-    }
 
     public enum Resolution
     {
@@ -24,13 +18,14 @@ public class VideoProxy : ResoniteMod
         Q720P,
         Q1080P,
         Q1440P,
+        Q2160P,
         QBest
     }
 
     public enum ProxyLocation
     {
         AU,
-        US,
+        NA,
         CUSTOM
     }
 
@@ -39,12 +34,13 @@ public class VideoProxy : ResoniteMod
     public override string Version => "0.0.1";
 
     public static ModConfiguration config;
-    [AutoRegisterConfigKey] private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("Whether or not to generate custom import button/s", "", () => true);
-    [AutoRegisterConfigKey] private static ModConfigurationKey<ProxyLocation> PROXY_LOCATION = new ModConfigurationKey<ProxyLocation>("Proxy Server Region", "", () => ProxyLocation.AU);
-    [AutoRegisterConfigKey] private static ModConfigurationKey<string> PROXY_URI = new ModConfigurationKey<string>("Proxy Server adress", "", () => "http://127.0.0.1:8080/video");
-    [AutoRegisterConfigKey] private static ModConfigurationKey<DebugLevel> DEBUG_LEVEL = new ModConfigurationKey<DebugLevel>("Debug Level", "", () => DebugLevel.Min);
-    [AutoRegisterConfigKey] private static ModConfigurationKey<Resolution> RESOLUTION = new ModConfigurationKey<Resolution>("Resolution", "", () => Resolution.Q1080P);
-    [AutoRegisterConfigKey] private static ModConfigurationKey<bool> FORCED = new ModConfigurationKey<bool>("Force h264 Codec", "", () => true);
+    [AutoRegisterConfigKey] private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabledToggle", "Whether or not to generate custom import button", () => true);
+    [AutoRegisterConfigKey] private static ModConfigurationKey<ProxyLocation> PROXY_LOCATION = new ModConfigurationKey<ProxyLocation>("serverRegion", "Proxy Server Region", () => ProxyLocation.AU);
+    [AutoRegisterConfigKey] private static ModConfigurationKey<string> PROXY_URI = new ModConfigurationKey<string>("serverAddress", "Proxy Server address", () => "http://127.0.0.1:8080/");
+    [AutoRegisterConfigKey] private static ModConfigurationKey<Resolution> RESOLUTION = new ModConfigurationKey<Resolution>("resolutionPreset", "Quality Preset", () => Resolution.Q720P);
+    [AutoRegisterConfigKey] private static ModConfigurationKey<dummy> RESOLUTION_WARNING = new ModConfigurationKey<dummy>("resolutionWarning", "<color=yellow>⚠</color> QBest will load the best quality it can so be careful when using this setting. <color=yellow>⚠</color>");
+    [AutoRegisterConfigKey] private static ModConfigurationKey<bool> FORCED = new ModConfigurationKey<bool>("forceCodecToggle", "Force h264 Codec", () => false);
+    [AutoRegisterConfigKey] private static ModConfigurationKey<dummy> CODEC_WARNING = new ModConfigurationKey<dummy>("codecWarning", "<color=yellow>⚠</color> Force h264 Codec only works for quality levels Q480P, Q720P, Q1080P <color=yellow>⚠</color>");
 
     public override void OnEngineInit()
     {
@@ -98,22 +94,10 @@ public class VideoProxy : ResoniteMod
         {
             Uri uri = null;
 
-            if (config.GetValue(DEBUG_LEVEL) != DebugLevel.None)
-            {
-                TextRenderer textRenderer = slot.AttachComponent<TextRenderer>();
-                switch (config.GetValue(DEBUG_LEVEL))
-                {
-                    case DebugLevel.Min:
-                        textRenderer.Text.Value = $"Loading video through proxy...";
-                        break;
-
-                    default:
-                        textRenderer.Text.Value = $"Loading video <color=green>{item.filePath}</color> through Video Proxy at <color=green>{config.GetValue(PROXY_URI)}</color>";
-                        break;
-                }
-                textRenderer.Bounded.Value = true;
-                textRenderer.Size.Value = 0.5f;
-            }
+            TextRenderer textRenderer = slot.AttachComponent<TextRenderer>();
+            textRenderer.Text.Value = $"Loading video through proxy...";
+            textRenderer.Bounded.Value = true;
+            textRenderer.Size.Value = 0.5f;
 
             using (HttpClient client = new HttpClient())
             {
@@ -123,84 +107,107 @@ public class VideoProxy : ResoniteMod
                     switch (config.GetValue(PROXY_LOCATION))
                     {
                         default:
+                        case ProxyLocation.CUSTOM:
+                            stringBuilder.Append(config.GetValue(PROXY_URI));
+                            break;
+
+                        case ProxyLocation.AU:
+                            stringBuilder.Append("https://ntau1.sveken.com");
+                            break;
+
+                        case ProxyLocation.NA:
                             stringBuilder.Append(config.GetValue(PROXY_URI));
                             break;
                     }
 
+                    if (config.GetValue(PROXY_URI).ToString().EndsWith("/")) stringBuilder.Append("reso/");
+                    else stringBuilder.Append("/reso/");
+
                     switch (config.GetValue(RESOLUTION))
                     {
                         case Resolution.Q480P:
-                            stringBuilder.Append("Q480");
+                            stringBuilder.Append("Q480P");
+                            if (config.GetValue(FORCED)) stringBuilder.Append("h264Forced");
                             break;
 
                         case Resolution.Q720P:
-                            stringBuilder.Append("Q720");
+                            stringBuilder.Append("Q720P");
+                            if (config.GetValue(FORCED)) stringBuilder.Append("h264Forced");
                             break;
 
-                        default:
                         case Resolution.Q1080P:
-                            stringBuilder.Append("Q1080");
+                            stringBuilder.Append("Q1080P");
+                            if (config.GetValue(FORCED)) stringBuilder.Append("h264Forced");
                             break;
 
                         case Resolution.Q1440P:
-                            stringBuilder.Append("Q1440");
+                            stringBuilder.Append("Q1440P");
+                            break;
+
+
+                        case Resolution.Q2160P:
+                            stringBuilder.Append("Q2160P");
                             break;
 
                         case Resolution.QBest:
                             stringBuilder.Append("QPoggers");
                             break;
-                    }
 
-                    if (config.GetValue(FORCED)) stringBuilder.Append("h264Forced");
+                        default:
+                            stringBuilder.Append("Q720P");
+                            break;
+                    }
 
                     string videoID = item.filePath.Replace("https://www.youtube.com/watch?v=", "").Replace("https://www.youtube.com/shorts/", "");
                     stringBuilder.Append($"/{videoID}");
 
-                    HttpResponseMessage response = await client.GetAsync(new Uri(stringBuilder.ToString()));
+                    Msg($"Attempting to load url: {stringBuilder.ToString()}");
 
-                    if (response.IsSuccessStatusCode)
+                    HttpResponseMessage response = await client.GetAsync(new Uri(stringBuilder.ToString()));
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    if (!content.StartsWith("error:") && response.IsSuccessStatusCode)
                     {
                         uri = new Uri(await response.Content.ReadAsStringAsync());
                         _ = slot.Engine;
                         VideoPlayerInterface videoInterface = await slot.SpawnEntity<VideoPlayerInterface, LegacyVideoPlayer>(FavoriteEntity.VideoPlayer);
                         videoInterface.InitializeEntity(item.itemName);
                         slot = videoInterface.Slot.GetObjectRoot();
-                        videoInterface.SetSource(uri, !config.GetValue(FORCED));
+                        videoInterface.SetSource(uri, true);
                         slot.Name = $"Video Proxy: {uri}";
+                        Msg($"Successfully found and loaded: {stringBuilder.ToString()}");
                         return Result.Success();
                     }
                     else
                     {
-                        TextRenderer textRenderer = slot.GetComponent<TextRenderer>();
+                        string error = $"Failed to load video: ({response.StatusCode}) {content}";
+
                         if (textRenderer != null)
                         {
-                            textRenderer.Text.Value = $"<color=red>Error Code: {response.StatusCode}";
+                            textRenderer.Text.Value = "<color=red>" + error;
                             textRenderer.SetupBoxCollider();
                             slot.AttachComponent<Grabbable>();
                         }
-                        return Result.Failure($"Error using video proxy: Status Code {response.StatusCode}");
+                        Error(error);
+                        return Result.Failure(error);
                     }
                 }
                 catch (Exception ex)
                 {
-                    TextRenderer textRenderer = slot.GetComponent<TextRenderer>();
+                    string error = $"Failed to load video: {ex.Message}";
+
                     if (textRenderer != null)
                     {
-                        switch (config.GetValue(DEBUG_LEVEL))
-                        {
-                            default:
-                            case DebugLevel.Min:
-                                textRenderer.Text.Value = $"<color=red>Failed to load: {ex.Message}";
-                                break;
+                        textRenderer.Text.Value = "<color=red>" + error;
+                        //textRenderer.Size.Value = 0.25f;
+                        //textRenderer.HorizontalAlign.Value = Elements.Assets.TextHorizontalAlignment.Left;
+                        //textRenderer.BoundsSize.Value = new float2(4, 2);
 
-                            case DebugLevel.Max:
-                                textRenderer.Text.Value = $"<color=red>Failed to load: {ex.StackTrace}";
-                                break;
-                        }
                         textRenderer.SetupBoxCollider();
                         slot.AttachComponent<Grabbable>();
                     }
-                    return Result.Failure($"Error using video proxy: {ex}");
+                    Error(error);
+                    return Result.Failure(error);
                 }
             }
         }

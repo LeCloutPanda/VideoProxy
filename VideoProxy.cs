88db24b237c07b65ccc5e7f4c5e0c4fd23a648c8
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2026-08-29 11:24:33 +1000

    Updated Regex, Copyright and .Net version

--- VideoProxy.cs
+++ VideoProxy.cs
@@ -5,10 +5,10 @@ using ResoniteModLoader;
 using System;
 using System.Threading.Tasks;
 using Elements.Core;
+using SkyFrost.Base;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
-using SkyFrost.Base;
 
 public class VideoProxy : ResoniteMod
 {
@@ -32,9 +32,9 @@ public class VideoProxy : ResoniteMod
 
     public override string Author => "LeCloutPanda & Sveken";
     public override string Name => "Video Proxy";
-    public override string Version => "1.3.0";
+    public override string Version => "1.4.0";
     public override string Link => "https://github.com/LeCloutPanda/VideoProxy";
-    private static string youtubeIdRegex = "(?:youtube\\.com\\/(?:[^\\/]+\\/.+\\/|(?:v|e(?:mbed)?|shorts)\\/|.*[?&]v=)|youtu\\.be\\/)([^\"&?\\/\\s]{11})";
+    private static string youtubeIdRegex = @"(.*?)(^|\/|v=)([a-z0-9_-]{11})(.*)?";
 
     public static ModConfiguration config;
     [AutoRegisterConfigKey] private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabledToggle", "Generate option to use VideoProxy when importing a video", () => true);
@@ -79,125 +79,7 @@ public class VideoProxy : ResoniteMod
                         Error(ex);
                     }
                 };
-
-                text = "Video Proxy: Import as Audio Clip\n<size=50%>(Not localized, convert to wav/orbis/etc to localize)";
-                Button proxyButton2 = uIBuilder.Button(in text);
-                proxyButton2.LocalPressed += (IButton button, ButtonEventData eventData) =>
-                {
-                    proxyButton2.Slot.Parent.GetComponentsInChildren<Button>().ForEach(button => button.Enabled = false);
-                    proxyButton2.LabelText = "Importing...";
-
-                    try
-                    {
-                        ImportAsAudioClip(__instance, proxyButton2);
-                    }
-                    catch (Exception ex)
-                    {
-                        proxyButton2.LabelText = $"<alpha=red>Failed to import video. Check logs.";
-                        Error(ex);
-                    }
-                };
             }
-        }
-
-        private static void ImportAsAudioClip(VideoImportDialog __instance, Button proxyButton) {
-            float3 b = float3.Zero;
-            float num = __instance.LocalUserRoot?.GlobalScale ?? 1f;
-
-            __instance.Paths.ForEach((item) =>
-            {
-                Slot slot = __instance.LocalUserSpace.AddSlot("Video Proxy(Local Import)");
-                float3 a = __instance.Slot.GlobalPosition;
-                slot.GlobalPosition = a + b;
-                slot.GlobalRotation = __instance.Slot.GlobalRotation;
-                a = float3.One;
-                slot.GlobalScale = num * a;
-                a = __instance.Slot.Right;
-                b += a;
-
-                UniversalImporter.UndoableImport(slot, async () =>
-                {
-                    string id = null;
-                    try
-                    {
-                        using (HttpClient client = new HttpClient())
-                        {
-                            Msg($"Attempting to process video URL: {item.filePath.ToString()}");
-                            Match match = Regex.Match(item.filePath.ToString(), youtubeIdRegex);
-
-                            if (match.Success)
-                            {
-                                id = match.Groups[1].Value;
-                            }
-                            else return Result.Failure("Failed to get video ID from URL");
-
-                            string baseUri = null;
-                            List<string> path = new List<string>();
-                            switch (config.GetValue(PROXY_LOCATION))
-                            {
-                                default:
-                                case ProxyLocation.CUSTOM:
-                                    baseUri = config.GetValue(PROXY_URI).Trim();
-                                    break;
-
-                                case ProxyLocation.Australia:
-                                    baseUri = "https://ntau1.sveken.com";
-                                    break;
-
-                                case ProxyLocation.NorthAmerica:
-                                    baseUri = "https://ntna1.sveken.com";
-                                    break;
-                            }
-
-                            UriBuilder builder = new UriBuilder(baseUri);
-
-                            path.Add("reso");
-                            path.Add("oggvorbis");
-                            path.Add(id);
-                            builder.Path = string.Join("/", path);
-
-                            Msg($"Attempting to fetch URL: {builder.Uri.ToString()}");
-
-                            HttpResponseMessage response = await client.GetAsync(builder.Uri);
-                            string content = await response.Content.ReadAsStringAsync();
-
-                            if (!content.ToLower().StartsWith("error:") && response.IsSuccessStatusCode)
-                            {
-                                Msg($"Importing as Audio Clip: {builder.Uri}");
-                                float3 b = float3.Zero;
-                                float num = proxyButton.LocalUserRoot?.GlobalScale ?? 1f;
-                                await default(ToWorld);
-                                Slot slot = proxyButton.LocalUserSpace.AddSlot("Video Proxy");
-                                float3 a = proxyButton.Slot.GlobalPosition;
-                                slot.GlobalPosition = a + b;
-                                slot.GlobalRotation = proxyButton.Slot.GlobalRotation;
-                                a = float3.One;
-                                slot.GlobalScale = num * a;
-                                a = proxyButton.Slot.Right;
-                                b += a;
-
-                                AudioPlayerInterface audioPlayer = await slot.SpawnEntity<AudioPlayerInterface, LegacyAudioPlayer>(FavoriteEntity.AudioPlayer);
-                                audioPlayer.InitializeEntity(System.IO.Path.GetFileName(content));
-                                audioPlayer.SetSource(new Uri(content));
-                                audioPlayer.SetType(AudioTypeGroup.Multimedia, false, 0f, 0f);
-
-                                __instance.Slot.Destroy();
-                                return Result.Success();
-                            }
-                            else
-                            {
-                                string error = $"Failed fetching video URL: ({response.StatusCode}) {content}";
-                                proxyButton.LabelText = $"<alpha=red>{error}";
-                                return Result.Failure(error);
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        return Result.Failure(ex.Message);
-                    }
-                });
-            });
         }
 
         private static void TryImport(VideoImportDialog __instance, Button proxyButton)
@@ -226,17 +108,15 @@ public class VideoProxy : ResoniteMod
             try {
                 using (HttpClient client = new HttpClient())
                 {
-                    Msg($"Attempting to process video URL: {item.filePath.ToString()}");
-                    Match match = Regex.Match(item.filePath.ToString(), youtubeIdRegex);
+                    Msg($"Attempting to process video URL: {item.filePath}");
+                    Match match = Regex.Match(item.filePath.ToString(), youtubeIdRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-                    if (match.Success)
+                    if (match.Success) 
                     {
-                        id = match.Groups[1].Value;
-                    } 
-                    else 
-                    {
-                        return Result.Failure("Failed to get video ID from URL");
+                        id = match.Groups[3].Value;
+                        Msg($"Found video id: {id}");
                     }
+                    else return Result.Failure("Failed to get video ID from URL");
 
                     string baseUri = null;
                     List<string> path = new List<string>();
